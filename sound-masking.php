e9a5afde569db5e7365ce8753a9a5dd31565819c
AbubakarCinnova <amahboob@cinnova.com>
2024-08-13 14:55:01 +0500

    Update sound-masking.php

--- sound-masking.php
+++ sound-masking.php
@@ -165,6 +165,7 @@
                                     <img src="images/misc/services-2.webp" class="img-fluid rounded-20px mb-4" alt="">
                                     <img src="images/misc/services-3.webp" class="img-fluid rounded-20px" alt="">
                                 </div>
+
                             </div>
 
                             <div class="spacer-double"></div>
