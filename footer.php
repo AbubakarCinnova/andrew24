e0cb970d5a6ad8e3996cde649e11c67b051ebb0d
AbubakarCinnova <amahboob@cinnova.com>
2024-08-27 12:19:53 +0500

    updated

--- footer.php
+++ footer.php
@@ -98,32 +98,7 @@
    <script src="form.js"></script>
    <script src="recaptcha.js"></script>
 
-   <script>
-jQuery(document).ready(function() {
-    // Hide all sub-menus initially
-    jQuery('#mainmenu .sub-menu').hide();
 
-    // Toggle the sub-menu on hover with a slide effect
-    jQuery('#mainmenu li.menu-item-has-children').hover(
-        function() {
-            jQuery(this).find('.sub-menu').first().stop(true, true).slideDown(300); // Slide down over 300ms
-        },
-        function() {
-            jQuery(this).find('.sub-menu').first().stop(true, true).slideUp(300); // Slide up over 300ms
-        }
-    );
-
-    // Ensure that hovering over sub-menu items doesn't immediately close the parent menu
-    jQuery('#mainmenu li.menu-item-has-children .sub-menu li').hover(
-        function() {
-            jQuery(this).parents('.sub-menu').stop(true, true).show(); // Keep the parent sub-menu open
-        },
-        function() {
-            // No action needed on mouse leave for sub-items
-        }
-    );
-});
-   </script>
 
    </body>
 
