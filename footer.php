e992abb41fd5b18e656e240f4da46e4af450c890
AbubakarCinnova <amahboob@cinnova.com>
2024-08-27 14:42:36 +0500

    sidney updated

--- footer.php
+++ footer.php
@@ -31,6 +31,7 @@
                                    <li><a href="columbus.php">Columbus</a> </li>
                                    <li><a href="mansfield.php">Mansfield</a> </li>
                                    <li><a href="cincinnati.php">Cincinnati</a> </li>
+                                   <li><a href="sidney.php">Sidney</a> </li>
                                </ul>
                            </div>
                        </div>
